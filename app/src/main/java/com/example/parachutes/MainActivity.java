1ae3d464807c318f82092891cb021a6f0048595e
Roees <vakrat@gmail.com>
2019-12-14 09:47:46 +0200

    with basic rendering

--- app/src/main/java/com/example/parachutes/MainActivity.java
+++ app/src/main/java/com/example/parachutes/MainActivity.java
@@ -3,21 +3,23 @@ package com.example.parachutes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private static int BOARD_HEIGHT = 6;
-    private static int BOARD_WIDTH = 6;
+    private static int BOARD_HEIGHT = 8;
+    private static int BOARD_WIDTH = 10;
     private Engine engine = new EngineImp(BOARD_HEIGHT, BOARD_WIDTH);
+    private ImageView[][] imageViews = new ImageView[BOARD_HEIGHT][BOARD_WIDTH];
 
     ImageButton leftButton;
     ImageButton rightButton;
-    GridLayout grid;
 
     void debugPrintBoard(){
         boolean[][] board = Board.getInstance().getBoard();
@@ -31,21 +33,34 @@ public class MainActivity extends AppCompatActivity {
         Log.e("test", "====================");
     }
 
-    void debugRender() {
-        new Thread(new Runnable() {
-            public void run(){
-                while (true){
-                    debugPrintBoard();
-                    try {
-                        Thread.sleep(2000);
-                    }
-                    catch (InterruptedException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
+    void printBoard(){
+        boolean[][] board = Board.getInstance().getBoard();
+        for(int i = 0; i < BOARD_HEIGHT; ++i) {
+            for (int j = 0; j < BOARD_WIDTH; ++j){
+                if(board[i][j]) imageViews[i][j].setVisibility(View.VISIBLE);
+                else imageViews[i][j].setVisibility(View.INVISIBLE);
             }
-        }).start();
+        }
+    }
+
+    void render() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                printBoard();
+                handler.postDelayed(this, 1000);
+            }
+        }, 1000);
+    }
+
+    void initImageViews(){
+        for(int i = 0; i < BOARD_HEIGHT; ++i)
+            for(int j=0; j < BOARD_WIDTH; ++j){
+                String img = "_" + i + "_" + j;
+                int id = getResources().getIdentifier(img, "id", getPackageName());
+                imageViews[i][j] = findViewById(id);
+            }
     }
 
     @Override
@@ -53,11 +68,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Board.getInstance().setSize(BOARD_HEIGHT, BOARD_WIDTH);
+        initImageViews();
         engine.run();
-        debugPrintBoard();
         leftButton = findViewById(R.id.leftButton);
         rightButton = findViewById(R.id.rightButton);
-        grid = findViewById(R.id.grid);
+        render();
 
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
